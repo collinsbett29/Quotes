8cd332cbc9ede718d989ed342cc12ae33be1dd8b
collinsbett29 <collinsbett29@gmail.com>
2019-09-23 17:13:13 +0300

    update new files

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,10 +9,10 @@ import { Quote} from'../quote'
 export class QuoteComponent implements OnInit {
   
   quotes:Quote[] = [
-    new Quote(1,'Be so good they can’t ignore you.','Don`t be too good to anybody','William Shakespeare','Collins',new Date(2020,3,14)),
-    new Quote(2,'The only joy in the world is to begin','Joy,the greatest thing ever','Arthur Conan Doyle','Kevin',new Date(2019,6,9)),
-    new Quote(3,'Happiness depends upon ourselves.','Always be happy',' Leo Tolstoy','Emmanuel',new Date(2022,1,12)),
-    new Quote(4,'Happiness is a state of activity.','You should exercise happiness','Edgar Allan Poe','Deborah',new Date(2019,0,18))
+    new Quote(1,'Be so good they can’t ignore you.','Don`t be too good to anybody','William Shakespeare','Collins',new Date(2020,3,14),"","0"),
+    new Quote(2,'The only joy in the world is to begin','Joy,the greatest thing ever','Arthur Conan Doyle','Kevin',new Date(2019,6,9),"0","",),
+    new Quote(3,'Happiness depends upon ourselves.','Always be happy',' Leo Tolstoy','Emmanuel',new Date(2022,1,12),"0",""),
+    new Quote(4,'Happiness is a state of activity.','You should exercise happiness','Edgar Allan Poe','Deborah',new Date(2019,0,18),"0","")
   
   ];
 
